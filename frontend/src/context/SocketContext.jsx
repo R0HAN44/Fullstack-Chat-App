19e873da7d2d0dcec96f72796c6549de2e4e86da
Rohan <96730573+R0HAN44@users.noreply.github.com>
2024-03-20 17:26:05 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://fullstack-chat-app-eq7p.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
